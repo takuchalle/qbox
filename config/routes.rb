4be536670451e51e06ea6d4d9bf8a8cee780da23
Takuya OHASHI <tky.ohashi@gmail.com>
2018-11-26 13:55:21 +0900

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'questions#new'
+
   get 'questions/new'
   get 'questions/show'
   get 'questions/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
