9de132aa6617b7e278c7bc4b48dfbc2902f7a8b7
Takuya OHASHI <tky.ohashi@gmail.com>
2018-11-27 15:27:43 +0900

    update test to pass

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -2,18 +2,12 @@ require 'test_helper'
 
 class QuestionsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get questions_new_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get questions_show_url
+    get new_question_url
     assert_response :success
   end
 
   test "should get index" do
-    get questions_index_url
+    get questions_url
     assert_response :success
   end
-
 end
