afa9deab0e438ff8b302b6847a279231ebc35394
Takuya OHASHI <tky.ohashi@gmail.com>
2018-11-27 12:08:56 +0900

    add create question action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,17 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
   def new
     @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -10,5 +21,11 @@ class QuestionsController < ApplicationController
   def index
     @question  = Question.new
     @questions = Question.all
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:content)
   end
 end
