a08328a36406c611474e6a086c3cf2de3d39c61b
Takuya OHASHI <tky.ohashi@gmail.com>
2018-11-28 13:11:36 +0900

    :muscle: Add secure_password to admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,6 @@
 class Admin < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 18 }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
