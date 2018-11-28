6cf1575c4eb832246392b1f9a0632012ad8dfb74
Takuya OHASHI <tky.ohashi@gmail.com>
2018-11-28 13:03:05 +0900

    :muscle: add admin model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_045443) do
+ActiveRecord::Schema.define(version: 2018_11_28_035906) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.text "content"
