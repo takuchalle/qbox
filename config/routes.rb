32cc81923fc993d20b36388ba7d615fc8de96f51
Takuya OHASHI <tky.ohashi@gmail.com>
2018-11-28 18:30:39 +0900

    :muscle: generate sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get 'admin/register', to: 'admins#new'
   post 'admin/register', to: 'admins#create'
+  get 'admin/login', to: 'sessions#new'
   resources :questions, only: %i[new show index create delete], param: :token
 end
