f56faa632e679ac621da1616b2afb7d52a63fa90
Takuya OHASHI <tky.ohashi@gmail.com>
2018-11-28 13:30:40 +0900

    :white_check_mark: Add Admin model test
     - only basic case

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -1,7 +1,33 @@
+# coding: utf-8
 require 'test_helper'
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @admin = Admin.new(name: "hoge", password: "hogehoge", password_confirmation: "hogehoge")
+  end
+
+  test 'should be valid' do
+    assert @admin.valid?
+  end
+
+  test 'should be valid when name is japanese' do
+    @admin.name = "ほげ"
+    assert @admin.valid?
+  end
+
+  test 'should not be valid when password is not match' do
+    @admin.password_confirmation = "fugafuga"
+    assert_not @admin.valid?
+  end
+
+  test 'should not be valid when password is short' do
+    @admin.password = "hoge"
+    @admin.password_confirmation = "hoge"
+    assert_not @admin.valid?
+  end
+
+  test 'should not be valid when name is long' do
+    @admin.name = "a" * 100
+    assert_not @admin.valid?
+  end
 end
