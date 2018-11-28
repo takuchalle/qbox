f6b10fcee217c6ecad05a1136458cba6a8c10710
Takuya OHASHI <tky.ohashi@gmail.com>
2018-11-28 14:23:48 +0900

    :white_check_mark: clear admin database before test

--- test/controllers/admins_controller_test.rb
+++ test/controllers/admins_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class AdminsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    Admin.all.each(&:destroy)
+  end
+
   test "should get new" do
     get admin_register_url
     assert_response :success
