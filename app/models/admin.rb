8c441507ce94521786a84bb9fa8a6ae28e8ab653
Takuya OHASHI <tky.ohashi@gmail.com>
2018-11-29 14:04:09 +0900

    :muscle: Create digest for test fixtures

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,4 +5,10 @@ class Admin < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def Admin.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+             BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
